edc215c36b5df020c6eee295fa244ae4dede7e01
Wendy Lisboa <53241067+wendy1893@users.noreply.github.com>
2020-01-14 14:50:12 -0400

    Proyecto Tienda de Camisetas PHP POO MVC MySQL

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+class Database{
+    public static function connect() {
+        $db = new mysqli('localhost', 'root', '', 'tienda_master');
+        $db->query("SELECT NAMES 'utf-8'");
+        return $db;
+        
+    }
+}
